f3303dbc40a3ab500dc8c0a9cb2dcb7a2ceea2e0
sontung1010 <sontungcodedao@gmail.com>
2024-12-03 21:06:09 -0500

    first commit

---
+++ setup_cifar10.ps1
@@ -0,0 +1,5 @@
+# wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+# tar -xf cifar-10-python.tar.gz
+
+curl -L -o cifar-10-python.tar.gz https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+tar -xf cifar-10-python.tar.gz
